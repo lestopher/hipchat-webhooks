47c53debedba2effa8b08dac80502b91e4cf2c60
christopher nguyen <christoph.nguyen@gmail.com>
2014-02-19 21:12:13 -0800

    Ripped out from a side project so it's more reusable

---
+++ room_message/room_message.go
@@ -0,0 +1,48 @@
+package room_message
+
+type RoomMessage struct {
+	Event string `json:"event"`
+	Item  Item   `json:"item"`
+}
+
+type Item struct {
+	Message       Message `json:"message"`
+	Room          Room    `json:"room"`
+	OauthClientId string  `json:"oauth_client_id"`
+	WebhookId     string  `json:"webhook_id"`
+}
+
+type Message struct {
+	Date string `json:"date"`
+	File string `json:"file"`
+	From struct {
+		Id    int32 `json:"id"`
+		Links struct {
+			Self string `json:"self"`
+		} `json:"links"`
+		MentionName string `json:"mention_name"`
+		Name        string `json:"name"`
+	} `json:"from"`
+	Id       int32     `json:"id"`
+	Mentions []Mention `json:"mentions"`
+	Message  string    `json:"message"`
+}
+
+type Mention struct {
+	Id    int32 `json:"id"`
+	Links struct {
+		Self string `json:"self"`
+	} `json:"links"`
+	MentionName string `json:"mention_name"`
+	Name        string `json:"name"`
+}
+
+type Room struct {
+	Id    int32 `json:"id"`
+	Links struct {
+		Members  string `json:"members"`
+		Self     string `json:"self"`
+		Webhooks string `json:"webhooks"`
+	} `json:"links"`
+	Name string `json:"name"`
+}
